df567f4d7fd7c8bed85006fe69c8d8d5c7c0ce68
Amanjeev Sethi <aj@amanjeev.com>
2017-04-15 16:52:01 -0400

    py27 so google stops pestering me

--- src/isitfridaythe13th.py
+++ src/isitfridaythe13th.py
@@ -1,20 +1,27 @@
 import datetime
+import webapp2
+from google.appengine.ext.webapp.util import run_wsgi_app
 
 now = datetime.datetime.now()
 
-print 'Content-Type: text/html'
-print ''
-print '<!DOCTYPE html>\
-        <html lang ="en">\
-        <head><title>Is it Friday the 13th?</title></head>\
-        <body style="text-align: center; padding-top: 200px;">\
-            <div class="content" style="font-weight: bold; font-size: 220px; font-family: Arial,sans-serif; text-decoration: none; color: black;">'
 
-if now.weekday() == 4 and now.day == 13:
-    print 'YES'
-else:
-    print 'NO'
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        isitfridaythe13th = 'NO'
+        if now.weekday() == 4 and now.day == 13:
+            isitfridaythe13th = 'YES'
 
-print '<div class="disclaimer" style="font-size:10px; ">All times are in UTC</div>'
-print '</div></body></html>'
+        self.response.out.write(
+            '<!DOCTYPE html><html lang ="en"><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /><title>Is it Friday the 13th?</title></head><body style="text-align: center; padding-top: 200px;"><div class="content" style="font-weight: bold; font-size: 220px; font-family: Arial,sans-serif; text-decoration: none; color: black;">' + isitfridaythe13th + '<div class="disclaimer" style="font-size:10px; ">All times are in UTC</div></div></body></html>')
 
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+
+
+def main():
+    run_wsgi_app(app)
+
+
+if __name__ == "__main__":
+    main()
