938c60baa37a9351dfad679ca02d8dd56e08a5bd
Amanjeev Sethi <aj@amanjeev.com>
2017-04-15 16:53:31 -0400

    debug

--- src/isitfridaythe13th.py
+++ src/isitfridaythe13th.py
@@ -16,7 +16,7 @@ class MainPage(webapp2.RequestHandler):
             '<!DOCTYPE html><html lang ="en"><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /><title>Is it Friday the 13th?</title></head><body style="text-align: center; padding-top: 200px;"><div class="content" style="font-weight: bold; font-size: 220px; font-family: Arial,sans-serif; text-decoration: none; color: black;">' + isitfridaythe13th + '<div class="disclaimer" style="font-size:10px; ">All times are in UTC</div></div></body></html>')
 
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage)])
 
 
 def main():
